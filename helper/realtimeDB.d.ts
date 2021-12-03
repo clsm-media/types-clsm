075ce6276e1d671fb2fddd7352d1cdf6dc1677eb
Brian Caimmi <bcaimmi@gmail.com>
2021-12-03 10:49:40 -0300

    release/v1.29.0

--- helper/realtimeDB.d.ts
+++ helper/realtimeDB.d.ts
@@ -7,6 +7,14 @@ declare namespace Helper {
         s?: number; // "s": if the user is speaking
         p?: string; // "p": user profile picture (URL)
         fn?: string; // "fn": user full name
+        m?: number; // "m": if the user is muted
+      }[]; // "h": hosts/guest participating in the conference (if any)
+      g?: {
+        i: string; // "i": user id
+        s?: number; // "s": if the user is speaking
+        p?: string; // "p": user profile picture (URL)
+        fn?: string; // "fn": user full name
+        m?: number; // "m": if the user is muted
       }[]; // "h": hosts/guest participating in the conference (if any)
       cc?: string; // "cc": closed captions
       l?: number; // "l": amount of listeners (if any)
