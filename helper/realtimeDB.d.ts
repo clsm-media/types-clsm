d40d511c08e9f85170b424021d7500ad5f050a4e
Brian Caimmi <bcaimmi@gmail.com>
2021-12-06 13:51:52 -0300

    release/v1.29.1

--- helper/realtimeDB.d.ts
+++ helper/realtimeDB.d.ts
@@ -1,14 +1,21 @@
 declare namespace Helper {  
   namespace RealtimeDB {
+    // t: current topic (if any)
     interface ISnapshot {
-      t?: string; // t: current topic (if any)
+      t?: {
+        i: string; // "i": topic id
+        n?: string; // "n": topic name
+        v?: string; // "v": topic voted times
+      }; 
+      // "h": hosts participating in the conference (if any)
       h?: {
         i: string; // "i": user id
         s?: number; // "s": if the user is speaking
         p?: string; // "p": user profile picture (URL)
         fn?: string; // "fn": user full name
         m?: number; // "m": if the user is muted
-      }[]; // "h": hosts/guest participating in the conference (if any)
+      }[];
+      // "g": guests participating in the conference (if any)
       g?: {
         i: string; // "i": user id
         s?: number; // "s": if the user is speaking
