dc3885f96832f865692f7fdff7bcbb9164134acc
Wilfredo Suarez <wilfredojsr2@gmail.com>
2021-12-30 18:10:17 -0300

    release/v1.39.0

--- entity/eventTypes.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace Entity {
-  enum IEventTypes {
-    JOINED = 'JOINED'
-  }
-}
---
+++ enum/main.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="./eventEnum.d.ts" />
+/// <reference path="./eventDetailsTypes.d.ts" />
